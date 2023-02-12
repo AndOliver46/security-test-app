3cb2f3d62822d1d3dde72e9b58e960ede6b93fdf
AndOliver46 <anderson_souza75@live.com>
2023-02-12 17:54:36 -0300

    Basic security class created

---
+++ src/main/java/com/andoliver46/SecutiryTestApp/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.andoliver46.SecutiryTestApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
