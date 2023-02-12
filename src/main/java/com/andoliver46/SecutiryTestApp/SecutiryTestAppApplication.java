44e7577fba3bf88ce8b7ea17db8cedd179eb97a9
AndOliver46 <anderson_souza75@live.com>
2023-02-12 17:02:23 -0300

    Project created

---
+++ src/main/java/com/andoliver46/SecutiryTestApp/SecutiryTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.andoliver46.SecutiryTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecutiryTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecutiryTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andoliver46/SecutiryTestApp/SecutiryTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andoliver46.SecutiryTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecutiryTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
