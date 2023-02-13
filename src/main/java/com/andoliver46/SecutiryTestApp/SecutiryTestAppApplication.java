f9682798e33e9848b4c049e1f4626144d4e94ce5
AndOliver46 <anderson_souza75@live.com>
2023-02-13 00:50:43 -0300

    SecurityConfig Jwt additions

--- src/main/java/com/andoliver46/SecutiryTestApp/SecutiryTestAppApplication.java
+++ src/main/java/com/andoliver46/SecutiryTestApp/SecutiryTestAppApplication.java
@@ -13,7 +13,7 @@ public class SecutiryTestAppApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SecutiryTestAppApplication.class, args);
-		System.out.println(new BCryptPasswordEncoder().encode("senha123"));
+		//System.out.println(new BCryptPasswordEncoder().encode("senha123"));
 	}
 
 }
--- src/main/java/com/andoliver46/SecutiryTestApp/config/SecurityConfig.java
+++ src/main/java/com/andoliver46/SecutiryTestApp/config/SecurityConfig.java
@@ -1,33 +1,58 @@
 package com.andoliver46.SecutiryTestApp.config;
 
+import com.andoliver46.SecutiryTestApp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthFilter;
+
+    @Autowired
+    private UserService userService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic();
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
 }
